004f8ec4344724d195c301950c812587eec8d06f
Maksym Kryvenko <maks1997ym@gmail.com>
2025-11-11 23:10:25 +0100

    Updated fields module

---
+++ src/models/fields.py
@@ -0,0 +1,128 @@
+from datetime import datetime
+import re
+from utils.validators import check_name, check_email, check_phone, check_date
+
+
+class Field:
+    """Base class for all fields."""
+
+    def __init__(self, value):
+        self._value = None
+        self.value = value
+
+    def __str__(self) -> str:
+        return str(self.value) if self.value else ""
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self.value})"
+
+    def __eq__(self, other) -> bool:
+        """Compare fields by value."""
+        if isinstance(other, Field):
+            return self.value == other.value
+        return self.value == other
+
+
+class Name(Field):
+    """Name field with validation."""
+
+    def __init__(self, value: str):
+        super().__init__(value)
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value: str):
+        if not check_name(new_value):
+            raise ValueError("Name cannot be empty and must not contain digits")
+        self._value = new_value.strip()
+
+
+class Phone(Field):
+    """Phone field with validation."""
+
+    def __init__(self, value: str):
+        super().__init__(value)
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value: str):
+        if not check_phone(new_value):
+            raise ValueError("Phone must be +380XX-XXX-XX-XX format")
+        self._value = self.__normalize_phone(new_value)
+
+    def __normalize_phone(phone_number: str) -> str:
+        """
+        Normalize a phone number to the format +380XXXXXXXXX.
+        :param phone_number: str The input phone number in various formats.
+
+        Return: str The normalized phone number.
+        """
+        phone_number = phone_number.strip().replace(" ", "")
+        phone_number = phone_number[phone_number.index("0") :]
+        found_number = re.fullmatch(
+            r"(\d{3})[\s\t()\n-]*(\d{3})[\s\t()\n-]*(\d{2})[\s\t()\n-]*(\d{2})",
+            phone_number,
+        )
+        found_number = f"+38{found_number[0]}"
+        _ = found_number[12]  # Ensure if the string is long enough
+        return found_number
+
+
+class Email(Field):
+    """Email field with validation."""
+
+    def __init__(self, value: str):
+        super().__init__(value)
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value: str):
+        if not check_email(new_value):
+            raise ValueError("Invalid email format")
+        self._value = new_value.strip().lower()
+
+
+class Address(Field):
+    """Address field."""
+
+    def __init__(self, value: str):
+        super().__init__(value)
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value: str):
+        self._value = new_value.strip() if new_value else None
+
+
+class Birthday(Field):
+    """Birthday field with string to datetime conversion."""
+
+    def __init__(self, value: str):
+        super().__init__(value)
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value: str):
+        if not check_date(new_value):
+            raise ValueError(
+                "Birthday must be in format DD.MM.YYYY and cannot be today or in the future"
+            )
+        self._value = datetime.strptime(new_value, "%d.%m.%Y")
+
+    def __str__(self) -> str:
+        return self._value.strftime("%d.%m.%Y") if self._value else ""
