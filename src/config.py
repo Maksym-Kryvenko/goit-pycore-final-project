115f50bfce9b88eee52771b8436e39b7d922b43c
Maksym Kryvenko <maks1997ym@gmail.com>
2025-11-11 22:44:09 +0100

    Updated save module with default value

---
+++ src/config.py
@@ -0,0 +1,5 @@
+# Default filename for storing the address book data
+DEFAULT_STORAGE_FILENAME = "data/addressbook.pkl"
+
+# Number of days to look ahead for upcoming birthdays
+UPCOMING_BIRTHDAYS_DAYS = 7
