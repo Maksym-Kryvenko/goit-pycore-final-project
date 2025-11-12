904fd97958a559c7c87ebf09c1ba4790e2ae907e
Oleksandr Hrynenko <grinenkoa@gmail.com>
2025-11-12 17:40:37 +0200

    fixed command all

--- src/cli/commands.py
+++ src/cli/commands.py
@@ -81,9 +81,8 @@ def get_phone(args, book: AddressBook):
 @input_error    
 def get_all_contacts(book: AddressBook):
     """ Return all contacts"""
-    print(f'{Fore.RED}Needed to do get all contact function!!!!!!!!!!!{Fore.RESET}')
-    # for record in book.data.values():
-    #     print(f"{Fore.GREEN}{record}{Fore.RESET}")
+    for record in book.data.values():
+        print(f"{Fore.GREEN}{record}{Fore.RESET}")
 
 
 @input_error
