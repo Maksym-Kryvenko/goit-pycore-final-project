e1558602321634ac7118f742bc286372c28d8944
Oleksandr Hrynenko <grinenkoa@gmail.com>
2025-11-12 16:21:09 +0200

    fixed command add

--- src/cli/commands.py
+++ src/cli/commands.py
@@ -37,13 +37,10 @@ def add_contact(args, book: AddressBook):
     name, phone, *_ = args
     record = book.search_contacts(name)
     message = f"{Fore.GREEN}Contact updated.{Fore.RESET}"
-    if record is None:
-        record = Contact(name)
+    if record == []:
+        record = Contact(name, phone)
         book.add_contact(record)
         message = f"{Fore.GREEN}Contact added.{Fore.RESET}"
-    # ToDo: create add_phone in Contact class
-    # if phone:
-    #     record.add_phone(phone)
     return message
 
 
--- src/utils/validators.py
+++ src/utils/validators.py
@@ -15,41 +15,44 @@ def check_email(email: str) -> bool:
     """
     Validate email
     """
-    pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
-    return re.match(pattern, email) is not None
+    if email is not None:
+        pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+        return re.match(pattern, email) is not None
+    else:
+        return False
 
 
 def check_phone(phone: str) -> bool:
     """
     Validate that the phone number is valid (Ukrainian format).
     """
-    phone_number = phone.strip().replace(" ", "")
-    phone_number = phone_number[phone_number.index("0") :]
-    pattern = r"(\d{3})[\s\t()\n-]*(\d{3})[\s\t()\n-]*(\d{2})[\s\t()\n-]*(\d{2})"
-    return re.fullmatch(pattern, phone_number) is not None
+    if phone is not None:
+        phone_number = phone.strip().replace(" ", "")
+        phone_number = phone_number[phone_number.index("0") :]
+        pattern = r"(\d{3})[\s\t()\n-]*(\d{3})[\s\t()\n-]*(\d{2})[\s\t()\n-]*(\d{2})"
+        return re.fullmatch(pattern, phone_number) is not None
+    else:
+        return False
 
 
 def check_date(date_str: str) -> bool:
     """
     Validate a date string in DD.MM.YYYY format, check if it's a real calendar date,
     and ensure the date is in the past (not today or in the future).
-
-    Args:
-        date_str (str): Date string to validate.
-
-    Returns:
-        bool: True if the date is valid, matches the format, and is only from the past, False otherwise.
     """
-    pattern = r"^\d{2}\.\d{2}\.\d{4}$"
-    if not re.match(pattern, date_str):
-        return False
-    try:
-        from datetime import datetime
+    if date_str is not None:
+        pattern = r"^\d{2}\.\d{2}\.\d{4}$"
+        if not re.match(pattern, date_str):
+            return False
+        try:
+            from datetime import datetime
 
-        date_obj = datetime.strptime(date_str, "%d.%m.%Y")
-        today = datetime.now()
-        if date_obj < today:
-            return True
-        return False
-    except ValueError:
+            date_obj = datetime.strptime(date_str, "%d.%m.%Y")
+            today = datetime.now()
+            if date_obj < today:
+                return True
+            return False
+        except ValueError:
+            return False
+    else:
         return False
